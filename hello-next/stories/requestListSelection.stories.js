10620c768d50f514067096996fbd9f89f05353f4
Steven Steven <steven.infinity29@gmail.com>
2019-04-18 00:11:01 -0400

    - Update Storybook docs Modified Dialog/Form and requestListSelection stories

--- hello-next/stories/requestListSelection.stories.js
+++ hello-next/stories/requestListSelection.stories.js
@@ -5,61 +5,130 @@ import { linkTo } from '@storybook/addon-links';
 import { action } from '@storybook/addon-actions';
 
 import RequestListSelection from '../components/Shared/RequestListSelection';
-import SRequestCard, {RequestCard} from '../components/Shared/RequestListSelection/RequestCard.tsx';
+import SRequestCard, { RequestCard } from '../components/Shared/RequestListSelection/RequestCard.tsx';
+import {FormType} from '../components/Shared/Forms/FormRenderHelper';
 
-const requestBuffer=[
-    {id:'1', date:'24 November 2019', title:'Orange', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Foo', fulfilled:true},
-    {id:'2', date:'24 November 2019', title:'Papaya', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Foo', fulfilled:true},
-    {id:'3', date:'24 November 2019', title:'Durian', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Foo', fulfilled:true},
-    {id:'4', date:'24 November 2019', title:'Mango', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Bar', fulfilled:true},
-    {id:'5', date:'24 November 2019', title:'Apple', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Bar', fulfilled:true},
-    {id:'6', date:'24 November 2019', title:'Pear', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Bar', fulfilled:true},
-    {id:'7', date:'24 November 2019', title:'Melon', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Foo', fulfilled:true},
-    {id:'8', date:'24 November 2019', title:'Lemon', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Bar', fulfilled:true},
-    {id:'9', date:'24 November 2019', title:'Grape', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Foo', fulfilled:true},
-    {id:'10', date:'24 November 2019', title:'Watermelon', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur project is', requesterUser:'Foo', fulfilled:true},
-    {id:'11', date:'24 November 2019', title:'Guava', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Foo', fulfilled:true},
-    {id:'12', date:'24 November 2019', title:'Pomegrate', description:'This Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur is', requesterUser:'Foo', fulfilled:true}
+const requestBuffer = [
+    {
+        "tags": [
+            "api",
+            "test"
+        ],
+        "name": "adipisicing",
+        "description": "id pariatur qui ea laboris",
+        "created_by": "Tester",
+        "status": "PENDING",
+        "price": {
+            "value": 4,
+            "currency": "CAD"
+        },
+        "created_at": "2019-04-04T00:43:04.588Z",
+        "id": "5ca55318f8e18100860471b2"
+    },
+    {
+        "tags": [
+            "api",
+            "test"
+        ],
+        "name": "do",
+        "description": "fugiat anim sint qui ex",
+        "created_by": "Tester",
+        "status": "COMPLETE",
+        "price": {
+            "value": 6,
+            "currency": "CAD"
+        },
+        "created_at": "2019-04-04T00:43:04.588Z",
+        "id": "5ca55318f8e18100860471b4"
+    },
+    {
+        "tags": [
+            "api",
+            "test"
+        ],
+        "name": "nulla",
+        "description": "qui amet consequat occaecat ad",
+        "created_by": "Tester",
+        "status": "PENDING",
+        "price": {
+            "value": 0,
+            "currency": "CAD"
+        },
+        "created_at": "2019-04-04T00:43:04.584Z",
+        "id": "5ca55318f8e18100860471ae"
+    },
+    {
+        "tags": [
+            "api",
+            "test"
+        ],
+        "name": "cillum",
+        "description": "aliqua incididunt officia commodo dolore",
+        "created_by": "Tester",
+        "status": "FULFILLED",
+        "price": {
+            "value": 5,
+            "currency": "CAD"
+        },
+        "created_at": "2019-04-04T00:43:04.588Z",
+        "id": "5ca55318f8e18100860471b3"
+    },
+    {
+        "tags": [
+            "api",
+            "test"
+        ],
+        "name": "elit",
+        "description": "ipsum cillum adipisicing Lorem officia",
+        "created_by": "Tester",
+        "status": "INCOMPLETE",
+        "price": {
+            "value": 3,
+            "currency": "CAD"
+        },
+        "created_at": "2019-04-04T00:43:04.587Z",
+        "id": "5ca55318f8e18100860471b1"
+    }
 ]
 
 storiesOf('RequestListSelection', module)
     .add('RequestListSelection', () => (
-            <RequestListSelection 
-                detailsHref={'/post?title='} 
-                requestBuffer={requestBuffer} 
-                onItemDelete={action('Item_Delete')} 
-                onItemCreate={action('Item_Create')}
-                favoriteRequests={['1','3','5']}
-                onAddFavoriteRequest={action('Add_to_favorite')}
-                onRemoveFavoriteRequest={action('Remove_from_favorite')}
-            />
-        ),
-        { info: {
-            text: "\
+        <RequestListSelection
+            detailsHref={'/post?title='}
+            requestBuffer={requestBuffer}
+            formType={FormType.REQUEST}
+            onItemDelete={action('Item_Delete')}
+            onItemCreate={action('Item_Create')}
+            favoriteRequests={['1', '3', '5']}
+            onAddFavoriteRequest={action('Add_to_favorite')}
+            onRemoveFavoriteRequest={action('Remove_from_favorite')}
+        />
+    ),
+        {
+            info: {
+                text: "\
                 Selection List of Request Cards. This component is a Grid list of cards that are wrapped with SelectableHOC to enhance selectable features on each card.\
                 See Selectable component for option\
                 ",
-            propTables: [RequestListSelection],
-        }}
+                propTables: [RequestListSelection],
+            }
+        }
     )
     .add('RequestCard', () => (
-            <SRequestCard
-                active={false}
-                onItemSelect={action('Item_Selected')}
-                date={'24 November 2019'}
-                requesterUser={"User1"}
-                fulfilled={true}
-                id={"id"} 
-                title={"title"} 
-                description={"description"}
-                isFavorite={true}
-                onAddFavoriteRequest= {action('Add_to_favorite')}
-                onRemoveFavoriteRequest= {action('Remove_from_favorite')}
-            />
-        ),
-        { info: {
-            text: 'RequestCard that display request info',
-            propTables: [RequestCard],
-            propTablesExclude: [SRequestCard],
-        }}
+        <SRequestCard
+            active={false}
+            onItemSelect={action('Item_Selected')}
+            request={requestBuffer[0]}
+            isFavorite={true}
+            onAddFavoriteRequest={action('Add_to_favorite')}
+            onRemoveFavoriteRequest={action('Remove_from_favorite')}
+        />
+    ),
+        {
+            info: {
+                text: 'RequestCard that display request info',
+                propTables: [RequestCard],
+                propTablesExclude: [SRequestCard],
+            }
+        }
     );
